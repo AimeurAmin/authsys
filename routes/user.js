e201fb8b5d144fbda5dde3d8089d04621f29ad8c
AimeurAmin <mohammedamin.aimeur@gmail.com>
2020-09-10 01:51:07 +0100

    adding /user/login route

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,7 @@ router.post('/signup',
     ],
     async (req, res) => {
         const errors = validationResult(req);
+        console.log(errors.array())
         if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array()});
 
         const {username, email} = req.body;
@@ -59,4 +60,64 @@ router.post('/signup',
     }
 )
 
-module.exports = router
+// Checking email using express-validator
+const mailcheck = () => check("email", "please enter a valid email.").isEmail();
+
+// Checking password using express-validator
+// const passwordcheck = () => check("password", "please enter a valid password.").isLength({min: 6}); 
+
+// Checking password manually
+const manualpasswordcheck = (req, res, next) => { 
+    if(req.body.password.length < 6) 
+        return res.send("please enter a valid password") 
+    else 
+        next()
+}
+
+// Route for user login
+router.post(
+    '/login',
+    mailcheck(),
+    manualpasswordcheck,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {email, password} = req.body;
+        try {
+            User.findOne({email}).then( async user => {
+                const isPasswordMatch = await bcrypt.compare(password, user.password);
+                console.log(isPasswordMatch);
+                if(isPasswordMatch){
+                    jwt.sign(
+                        {
+                            user: {
+                                id: user.id
+                            }
+                        },
+                        "secret",
+                        { expiresIn: 3600 },
+                        (err, token) => {
+                            if(err) throw err;
+                            res.status(200).json({token})
+                        }
+                    );
+                } else {
+                    return res.status(400).json({
+                        message: "Incorrect password."
+                    })
+                }
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: "Server Error"
+            });
+        }
+    }
+);
+
+
+module.exports = router;
